9c10e9eeaa62db9f432db7d9c39122984d538a3f
AmineHeroual <aminehero@protonmail.com>
2023-06-08 20:33:24 +0100

    change the login method part 13

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,7 @@ Route::namespace('Front')->group(function(){
 
 Route::namespace('Admin')->group(function(){
     Route::get('/dashboard/login', [AuthAdminController::class,'login'])->name('admin.login');
-    Route::put('/dashboard/do-login', [AuthAdminController::class,'doLogin'])->name('admin.doLogin');
+    Route::post('/dashboard/do-login', [AuthAdminController::class,'doLogin'])->name('admin.doLogin');
 
     Route::middleware('adminAuth:admin')->group(function(){
         Route::get('/dashboard/logout', [AuthAdminController::class,'logout'])->name('admin.logout');
